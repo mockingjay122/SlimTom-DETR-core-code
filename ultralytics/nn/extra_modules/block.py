392fd7e212f3fb166580d9fd864cd0586dac2322
mockingjay <gj2002196@icloud.com>
2026-01-05 20:28:00 +0800

    first commit

---
+++ ultralytics/nn/extra_modules/block.py
@@ -0,0 +1,137 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+from ..modules.conv import Conv
+from ..modules.block import  C2f
+from timm.layers import DropPath
+
+__all__ = ['ELA_HSFPN','C2f_DMB' ]
+
+
+class DMConv(nn.Module):
+    """ Dynamic Inception depthweise convolution
+    """
+
+    def __init__(self, in_channels, square_kernel_size=3, band_kernel_size=11):
+        super().__init__()
+        self.dwconv = nn.ModuleList([
+            nn.Conv2d(in_channels, in_channels, square_kernel_size, padding=square_kernel_size // 2,
+                      groups=in_channels),
+            nn.Conv2d(in_channels, in_channels, kernel_size=(1, band_kernel_size), padding=(0, band_kernel_size // 2),
+                      groups=in_channels),
+            nn.Conv2d(in_channels, in_channels, kernel_size=(band_kernel_size, 1), padding=(band_kernel_size // 2, 0),
+                      groups=in_channels)
+        ])
+
+        self.bn = nn.BatchNorm2d(in_channels)
+        self.act = nn.SiLU()
+
+        # Dynamic Kernel Weights
+        self.dkw = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_channels, in_channels * 3, 1)
+        )
+
+    def forward(self, x):
+        x_dkw = rearrange(self.dkw(x), 'bs (g ch) h w -> g bs ch h w', g=3)
+        x_dkw = F.softmax(x_dkw, dim=0)
+        x = torch.stack([self.dwconv[i](x) * x_dkw[i] for i in range(len(self.dwconv))]).sum(0)
+        return self.act(self.bn(x))
+
+
+class DM(nn.Module):
+    def __init__(self, channel=256, kernels=[3, 5]):
+        super().__init__()
+        self.groups = len(kernels)
+        min_ch = channel // 2
+
+        self.convs = nn.ModuleList([])
+        for ks in kernels:
+            self.convs.append(DMConv(min_ch, ks, ks * 3 + 2))
+        self.conv_1x1 = Conv(channel, channel, k=1)
+
+    def forward(self, x):
+        _, c, _, _ = x.size()
+        x_group = torch.split(x, [c // 2, c // 2], dim=1)
+        x_group = torch.cat([self.convs[i](x_group[i]) for i in range(len(self.convs))], dim=1)
+        x = self.conv_1x1(x_group)
+        return x
+
+
+class DMB(nn.Module):
+    def __init__(self, dim, drop_path=0.0):
+        super().__init__()
+        self.norm1 = nn.BatchNorm2d(dim)
+        self.norm2 = nn.BatchNorm2d(dim)
+        self.mixer =DM(dim)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.mlp = ConvolutionalGLU(dim)
+        layer_scale_init_value = 1e-2
+        self.layer_scale_1 = nn.Parameter(
+            layer_scale_init_value * torch.ones((dim)), requires_grad=True)
+        self.layer_scale_2 = nn.Parameter(
+            layer_scale_init_value * torch.ones((dim)), requires_grad=True)
+
+    def forward(self, x):
+        x = x + self.drop_path(self.layer_scale_1.unsqueeze(-1).unsqueeze(-1) * self.mixer(self.norm1(x)))
+        x = x + self.drop_path(self.layer_scale_2.unsqueeze(-1).unsqueeze(-1) * self.mlp(self.norm2(x)))
+        return x
+
+
+class C2f_DMB(C2f):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(DMB(self.c) for _ in range(n))
+
+
+class ELA_HSFPN(nn.Module):
+    def __init__(self, in_planes, flag=True):
+        super(ELA_HSFPN, self).__init__()
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+        self.conv1x1 = nn.Sequential(
+            nn.Conv1d(in_planes, in_planes, 7, padding=3),
+            nn.GroupNorm(16, in_planes),
+            nn.Sigmoid()
+        )
+        self.flag = flag
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+        x_h = self.conv1x1(self.pool_h(x).reshape((b, c, h))).reshape((b, c, h, 1))
+        x_w = self.conv1x1(self.pool_w(x).reshape((b, c, w))).reshape((b, c, 1, w))
+        return x * x_h * x_w if self.flag else x_h * x_w
+
+
+class ConvolutionalGLU(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.) -> None:
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        hidden_features = int(2 * hidden_features / 3)
+        self.fc1 = nn.Conv2d(in_features, hidden_features * 2, 1)
+        self.dwconv = nn.Sequential(
+            nn.Conv2d(hidden_features, hidden_features, kernel_size=3, stride=1, padding=1, bias=True,
+                      groups=hidden_features),
+            act_layer()
+        )
+        self.fc2 = nn.Conv2d(hidden_features, out_features, 1)
+        self.drop = nn.Dropout(drop)
+
+    # def forward(self, x):
+    #     x, v = self.fc1(x).chunk(2, dim=1)
+    #     x = self.dwconv(x) * v
+    #     x = self.drop(x)
+    #     x = self.fc2(x)
+    #     x = self.drop(x)
+    #     return x
+
+    def forward(self, x):
+        x_shortcut = x
+        x, v = self.fc1(x).chunk(2, dim=1)
+        x = self.dwconv(x) * v
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x_shortcut + x
